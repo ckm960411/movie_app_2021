9cc6dfe06b013551052fa874394ab6c2bc2e36f8
ckm960411 <a01098956117@gmail.com>
2021-12-12 13:13:20 +0900

    2011.12.12 최신 유지

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import "./Navigation.css"
+
+function Navigation() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  )
+}
+
+export default Navigation
---
+++ src/routes/About.js
@@ -0,0 +1,7 @@
+function About() {
+  return (
+    <div>About this page: Ibuilt it because I love movies.</div>
+  )
+}
+
+export default About
